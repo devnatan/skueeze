3a9a3aac0ccd59480a3b7e6d0c1ce7e359b23739
Natan <natanvnascimento@gmail.com>
2025-12-23 11:58:43 -0300

    Write test for 8, 16 and 32 bits compression and read

--- lib/common/test/me/devnatan/kompress/BitBufferTest.kt
+++ lib/common/test/me/devnatan/kompress/BitBufferTest.kt
@@ -24,4 +24,44 @@ class BitBufferTest {
             actual = buffer.readBits(1)
         )
     }
+
+    @Test
+    fun `write and read full`() {
+        val buffer = BitBuffer()
+        buffer.writeBits(0xFFu, 8)
+        assertEquals(
+            expected = 0xFFu,
+            actual = buffer.readBits(8)
+        )
+    }
+
+    @Test
+    fun `write and read 16`() {
+        val buffer = BitBuffer()
+        buffer.writeBits(0xABCDu, 16)
+        assertEquals(
+            expected = 0xABCDu,
+            actual = buffer.readBits(16)
+        )
+    }
+
+    @Test
+    fun `write and read 32`() {
+        val buffer = BitBuffer()
+        buffer.writeBits(0x12345678u, 32)
+        assertEquals(
+            expected = 0x12345678u,
+            actual = buffer.readBits(32)
+        )
+    }
+
+    @Test
+    fun `write and read 32 max`() {
+        val buffer = BitBuffer()
+        buffer.writeBits(0xFFFFFFFFu, 32)
+        assertEquals(
+            expected = 0xFFFFFFFFu,
+            actual = buffer.readBits(32)
+        )
+    }
 }
